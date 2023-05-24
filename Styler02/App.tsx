d980f54f0c99ee4f286d1cc7d44b2d1a9ddcc9a0
Anurag Banger <anuraagbanger@gmail.com>
2023-05-24 15:28:27 +1000

    new component

--- Styler02/App.tsx
+++ Styler02/App.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+import FlatCards from './components/FlatCards';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default App
+export default App;
