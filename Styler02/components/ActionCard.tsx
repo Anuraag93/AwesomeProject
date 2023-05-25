7e1cce71b4ca5544c85f0255686d3744679d7472
Anurag Banger <anuraagbanger@gmail.com>
2023-05-25 13:05:51 +1000

    action card with touchable opacity btn

---
+++ Styler02/components/ActionCard.tsx
@@ -0,0 +1,108 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+
+export default function ActionCard() {
+  function openWebsite(websiteLink: string) {
+    Linking.openURL(websiteLink);
+  }
+  return (
+    <View>
+      <Text style={styles.headingText}>Blog Card</Text>
+      <View style={[styles.card, styles.elevatedCard]}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headerText}>
+            What's new in Javascript 21 - ES12
+          </Text>
+        </View>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.bodyContainer}>
+          <Text numberOfLines={3}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Text>
+        </View>
+        <View style={styles.footerContainer}>
+          <TouchableOpacity onPress={() => openWebsite('https://google.com')}>
+            <Text style={styles.socialLinks}>Read More</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => openWebsite('https://google.com')}>
+            <Text style={styles.socialLinks}>Follow Me</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  card: {
+    width: 350,
+    height: 360,
+    borderRadius: 10,
+    marginVertical: 12,
+    marginHorizontal: 16,
+  },
+  elevatedCard: {
+    backgroundColor: '#E07C24',
+    elevation: 4,
+    shadowOffset: {width: 1, height: 1},
+    shadowColor: 'blue',
+    shadowOpacity: 0.4,
+  },
+  headingContainer: {
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  cardImage: {
+    height: 190,
+  },
+  bodyContainer: {
+    padding: 10,
+  },
+  footerContainer: {
+    padding: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  socialLinks: {
+    color: 'blue',
+    fontSize: 16,
+    padding: 8,
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 6,
+    borderRadius: 10,
+  },
+});
